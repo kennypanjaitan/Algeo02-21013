ed5274f06fc3e66809163e9f780dc5ddba7b8bb6
syrifaa <13521018@std.stei.itb.ac.id>
2022-11-12 22:12:30 +0700

    create mean and covarian

--- loadImage.py
+++ loadImage.py
@@ -2,27 +2,48 @@ import cv2
 import os
 import numpy as np
 
-def load_images_from_folder(folder):
+def covarian(matrixMean,folder): # cari matrix covarian
+    count = 0
+    for filename in os.listdir(folder):
+        mat = cv2.imread(os.path.join(folder,filename),0)
+        mat = cv2.resize(mat,(256,256))
+        count += 1
+        mat = mat - matrixMean
+        if (count == 1):
+            matNew = mat
+        else:
+            matNew = np.concatenate((matNew, mat), axis=1)
+    matNewTranspose = np.transpose(matNew)
+    matCovarian = np.matmul(matNew,matNewTranspose)
+    return matCovarian
+
+def mean(folder): # cari matrix mean
     count = 0
     rows, cols = (256,256)
     matrix = [[0 for i in range(cols)] for j in range(rows)]
     for filename in os.listdir(folder):
-        img = cv2.imread(os.path.join(folder,filename))
-        img.resize(256,256)
-        if img is not None:
-            count += 1
-            matrix = np.add(matrix,img)
-            # cv2.imshow('image',img)
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
-    print(count)
+        img = cv2.imread(os.path.join(folder,filename),0)
+        img = cv2.resize(img,(256,256))
+        count += 1
+        matrix = np.add(matrix,img)
+        # cv2.imshow('image',img)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+    # print(matrix)
     matrixMean = matrix * (1/count)
-    print(matrixMean)
+    matrixMean = matrixMean.astype(int)
+    # print(matrixMean)
+    return matrixMean
 
-def load_folder_from_folder(foldername):
-    for folder in os.listdir(foldername):
-        name = foldername+"/"+folder
-        load_images_from_folder(name)
-
-foldername = 'dataset'
-load_folder_from_folder(foldername)
+# main
+foldername = 'dataset' # nama folder datasetnya
+count = 0
+for folder in os.listdir(foldername):
+    name = foldername+"/"+folder
+    matrixMean = mean(name)
+    matrixCovarian = covarian(matrixMean,name)
+    # print(folder)
+    print(matrixCovarian)
+    # count += 1
+    # if (count == 2):
+    #     break
