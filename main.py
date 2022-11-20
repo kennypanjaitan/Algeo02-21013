7a1134e8df16c1437db4867affd6bef0e500cfd8
syrifaa <13521018@std.stei.itb.ac.id>
2022-11-20 16:08:59 +0700

    edit main

--- main.py
+++ main.py
@@ -1,133 +1,145 @@
-import cv2
 import os
+import cv2
 import numpy as np
 from qrDecomposition import *
-from numpy import linalg as LA
+from euclideanDistance import *
 
-def mean(folder): # cari matrix mean
-    count = 0
-    matrix = [[0 for i in range(256)] for j in range(256)]
-    for filename in os.listdir(folder):
-        img = cv2.imread(os.path.join(folder,filename),0)
-        img = cv2.resize(img,(256,256))
-        img = img[50:206, 50:206]
-        img = cv2.resize(img,(256,256))
-        # print(img)
-        # cv2.imshow('imageSelisih',img)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-        count += 1
-        matrix = np.add(matrix,img)
-    matrixMean = matrix * (1/count)
-    matrixMean = matrixMean.astype(np.uint8)
-    return matrixMean
+width = 256
+height = 256
 
-def selisih(mean):
-    count = 0
-    matrix = []
-    for filename in os.listdir(folder):
-        img = cv2.imread(os.path.join(folder,filename),0)
-        img = cv2.resize(img,(256,256))
-        img = img[50:206, 50:206]
-        img = cv2.resize(img,(256,256))
-        count += 1
-        selisih = np.subtract(img,mean)
-        # selisih = selisih.astype(np.uint8)
-        # print(selisih)
-        # cv2.imshow('imageSelisih',selisih)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-        matrix.append(selisih)
-        if (count == 1):
-            matNew = selisih
-        else:
-            matNew = np.concatenate((matNew, selisih), axis=1)
-    matrix = np.array(matrix)
-    matCovarian = np.cov(matNew)
-    # matCovarian = matNew @ np.transpose(matNew)
-    # matCovarian = matCovarian.astype(np.uint8)
-    # print(matCovarian)
-    # cv2.imshow('imageCov',matCovarian)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    return matCovarian, matrix
-    # return matrix
-
-
-folder = 'dataset' # nama folder datasetnya
-matrixMean = mean(folder)
-# print(matrixMean)
-# cv2.imshow('imageMean',matrixMean)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-matCovarian, selisih = selisih(matrixMean)
-# matrix = selisih(matrixMean)
-# for i in range (len(matrix)):
-#     img = matrix[i]
-#     cv2.imshow('imageSelisih',img)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-# cov_matrix = cov_matrix.astype(np.uint8)
-# print(cov_matrix)
-# cv2.imshow('imageCov',cov_matrix)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-val, vec = find_eig_qr(matCovarian)
-# print(val)
-# print(vec)
-
-eigenFace = []
-for i in range (len(selisih)):
-    matEigFace = vec @ selisih[i]
-    # matEigFace = matEigFace.astype(np.uint8)
-    # print(i)
-    # cv2.imshow('image',matEigFace)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    eigenFace.append(matEigFace)
-eigenFace = np.array(eigenFace)
-
-input = 'testcase/Adriana Lima225_126.jpg'
-# rows, cols = (256,256)
-# matAdd = [[100 for i in range(cols)] for j in range(rows)]
-matImg = cv2.imread(input,0)
-matImg = cv2.resize(matImg,(256,256))
-matImg = matImg[50:206, 50:206]
-matImg1 = cv2.resize(matImg,(256,256))
-# matImg1 = np.add(matAdd,matImg1)
-# print(matImg1)
-# cv2.imshow('image',matImg1)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-matImg2 = np.subtract(matImg1,matrixMean)
-# matImg2 = matImg2.astype(np.uint8)
-# matImg = np.round(matImg)
-matEigImg = vec @ matImg2
-# matEigImg = matEigImg.astype(np.uint8)
-# matEigImg = matEigImg.astype(np.uint8)
-# print(matEigImg)
-# cv2.imshow('image3',matEigImg)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-count = 0
-for i in range (len(eigenFace)):
-    dist = np.linalg.norm(matEigImg-eigenFace[i])
-    if (i == 0) :
-        min = dist
-        ayey = i
-    else :
-        if (min > dist) :
-            min = dist
-            ayey = i
-
-# print(min)
-# cv2.imshow('image2',eigenFace[ayey])
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-n = 0
+# menyimpan nama training image dalam sebuah array
+folder = 'datasett'
+img_names = []
 for filename in os.listdir(folder):
-    if (n == ayey):
-        print(filename)
-    n += 1
+    img_names.append(filename)
+
+
+# membaca training image dan menyimpan dalam array
+training_img   = np.ndarray(shape=(len(img_names), height*width))
+for i in range(len(img_names)):
+    img = cv2.imread(os.path.join(folder,img_names[i]),0)
+    img = cv2.resize(img,(height,width))
+    training_img[i] = img.reshape(1,height*width)
+
+
+#  menghitung mean
+matMean = np.zeros((1,height*width))
+for i in range (len(img_names)):
+    matMean = np.add(matMean,(1/(len(img_names)))*training_img[i])
+# print(matMean)
+# haha = np.reshape(matMean.astype(np.uint8),(height,width))
+# cv2.imshow('image',haha)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+# menghitung selisih  / normalize vector
+normalize_training_img = np.ndarray(shape=(len(img_names), height*width))
+for i in range(len(img_names)):
+    normalize_training_img[i] = np.subtract(training_img[i],matMean)
+    # haha = np.reshape(normalize_training_img[i].astype(np.uint8),(height,width))
+    # cv2.imshow('image',haha)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+
+#  membuat matriks A untuk menghitung Covarian
+A = np.zeros((len(img_names),height*width))
+for i in range (len(img_names)):
+    A[i,:] = normalize_training_img[i]
+    # haha = np.reshape(A[i,:].astype(np.uint8),(height,width))
+    # cv2.imshow('image',haha)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+
+# menghitung Covarian
+matCovarian = A @ (np.transpose(A))
+
+
+# mencari eigen value dan eigen vector
+val, vec = find_eig_qr(matCovarian)
+
+
+# mencari eigen face
+normalize_vector = np.zeros((len(img_names),height*width))
+for i in range(len(img_names)):
+    normalize_vector[i] = np.transpose(vec)[:,len(vec)-1-i] @ A
+    # haha = np.reshape(eigenFace[i+1].astype(np.uint8),(height,width))
+    # cv2.imshow('image',haha)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+eigenFace = normalize_vector.copy()
+for i in range(len(img_names)):
+    eigenFace[i] = normalize_vector[len(img_names)-1-i] / euclidean_distance((vec[(len(img_names)-1)-i]),2)
+    # haha = np.reshape(eigenFace[i].astype(np.uint8),(height,width))
+    # cv2.imshow('image',haha)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+
+# memilih eigen vector yang memiliki eigen value >= 1
+# count = 0
+# for i in range (len(val)):
+#     if (val[i] > 1):
+#         count += 1
+# print(count) = 104
+# print(len(val)) = 105
+
+eigenfaces_used = 104
+
+
+# mencari weight
+w = np.zeros((len(eigenFace),eigenfaces_used))
+for i in range (len(eigenFace)):
+    for j in range (eigenfaces_used):
+        w[i,j] = eigenFace[j] @ normalize_training_img[i]
+    
+
+'''_____________________________________________________________________________'''
+
+# membaca test image
+foldername = 'testcase/Dwayne Johnson99_1699.jpg'
+img = cv2.imread(os.path.join(foldername),0)
+img = cv2.resize(img,(width,height))
+test_img = img.reshape(1,height*width)
+
+
+# menghitung selisih  / normalize vector
+normalize_test_img = np.subtract(test_img,matMean)
+# haha = np.reshape(normalize_uface_vector.astype(np.uint8),(height,width))
+# cv2.imshow('image',haha)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# print(haha)
+
+
+# mencari weight
+w_test = np.zeros((1,eigenfaces_used))
+for i in range (eigenfaces_used):
+    w_test[0,i] = eigenFace[i] @ normalize_test_img[0]
+
+
+# mencari euclidean distance minimum
+dist = np.zeros((1,len(img_names)))
+for i in range(len(img_names)):
+    dist[0,i] = euclidean_distance(w_test[0,:],w[i,:])
+    if (i == 0) :
+        min = dist[0,i]
+        ayey = i
+    else:
+        if (min > dist[0,i]):
+            min = dist[0,i]
+            ayey = i
+print(min)
+
+
+# mencari nama dengan distance minimum dan menampilkan training imagenya
+for i in range(len(img_names)):
+    if (i == ayey):
+        print(img_names[i])
+        haha = np.reshape(training_img[i].astype(np.uint8),(height,width))
+        cv2.imshow('image',haha)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
